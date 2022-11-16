a853fc325d4c281248e030e590193dbe4b4574be
Erica Ferreira <ericacunhaferreira0@gmail.com>
2022-11-15 22:27:23 -0300

    fix: fix enconding

--- Quizzes/Parsing.rb
+++ Quizzes/Parsing.rb
@@ -1,7 +1,10 @@
-File.foreach("engsoft.txt", encoding: 'windows-1252') { |each_line|
+file = File.open("engsoft.txt", "r:ISO-8859-1:UTF-8")
+
+file.each{ |each_line|
     string = each_line.scan(/\d{2}\/{1}\d{7}\s{4}.*/)
     if string.length > 0
         puts string[0].gsub(/\s{4}/,"\t")
     end
 }
 
+file.close
